d2e1978adafc84825105e5aa7bdd4140506996cb
linwang <w_lin1025@163.com>
2017-07-01 15:33:38 +0800

    Find Next Permutation

---
+++ Find_NextPermutation.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+void swap(char *a , char *b)
+{
+  char temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+void Reverse(char * a , char *b)
+{
+  while(a<b)
+  {
+    swap(a,b);
+    a++;
+    b--;
+  }
+}
+
+void showstr(char a[],int len)
+{
+  printf("%s\n",a);
+}
+void FindNextPermutation(char a[],int len)
+{
+  if(NULL == a)
+  {
+    return;
+  }
+  char *pEnd = a + len;
+  char *p , *q , *pFind;
+  pEnd--;
+  p = pEnd;
+  while(p!=a)
+  {
+    q = p;
+    --p;
+    if(*p < *q)
+    {
+      pFind = pEnd;
+      while(*pFind <= *p)
+      {
+        pFind--;
+      }
+      swap(pFind,p);
+      Reverse(q,pEnd);
+      showstr(a,len);
+      return ;
+    }
+  }
+  Reverse(p,pEnd);
+  return;
+}
+
+
+int main()
+{
+  char str[] = "21543";
+  int  len   = sizeof(str) - 2;
+  FindNextPermutation(str,len);
+}
